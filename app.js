1371336ced8f57866c653ca9388ee383dfde9faa
Derek Bredensteiner <elspoono@gmail.com>
2011-09-15 17:06:21 -0700

    fix redirects

--- app.js
+++ app.js
@@ -1945,11 +1945,11 @@ app.get('/_:partial', function(req, res, next){
   })
 })
 
-app.get(/^(?!(\/images|\/stylesheets|\/javascripts)).*/, function(req, res, next){
+app.get(/^(?!(\/images|\/stylesheets|\/javascripts)).*$/, function(req, res, next){
   res.send('',{
       Location:'/'
   },301);
-})
+});
 
 
 app.get('/robots.txt', function(req, res, next){
