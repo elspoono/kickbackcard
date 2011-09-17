9943cf4a4c39730cba9c7c146de3c4390c5b30d8
Derek Bredensteiner <elspoono@gmail.com>
2011-09-17 04:51:07 -0700

    fix redirects

--- app.js
+++ app.js
@@ -1861,7 +1861,7 @@ var findNearVendors = function(req, res, next){
 
   Vendor.find(
     {
-      coordinates : { $near : [params.latitude, params.longitude], $maxDistance: 2 },
+      coordinates : { $near : [params.latitude, params.longitude], $maxDistance: 0.5 },
       _id : { $nin : req.mapClient.vendor_ids},
       active: true
     },
@@ -1870,7 +1870,7 @@ var findNearVendors = function(req, res, next){
     function(err, vendors){
       Vendor.find(
         {
-          coordinates : { $near : [params.latitude, params.longitude], $maxDistance: 2 },
+          coordinates : { $near : [params.latitude, params.longitude], $maxDistance: 0.5 },
           active: true
         },
         ['name'],
