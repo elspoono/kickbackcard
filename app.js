e76fc83eabc17292ad1be672bac82e2b3b9a6482
Derek Bredensteiner <elspoono@gmail.com>
2011-10-09 13:35:33 -0700

    transports

--- app.js
+++ app.js
@@ -2397,7 +2397,7 @@ app.get('/dashboard', securedAreaVendor, function(req, res, next){
 });
 io.configure(function (){
 
-  io.set('transports', ['flashsocket']);
+  io.set('transports', ['xhr-polling', 'json-polling', 'jsonp-polling', 'htmlfile']);
 
   io.set('authorization', function (data, next) {
     var cookies = data.headers.cookie.split(/; */);
