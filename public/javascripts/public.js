dd6c95f451d5f2c4d60a372f8f5f52743dcf7360
Derek Bredensteiner <elspoono@gmail.com>
2011-10-11 01:40:02 -0700

    socket.io some more

--- public/javascripts/public.js
+++ public/javascripts/public.js
@@ -13,21 +13,16 @@ _gaq.push(['_trackPageview']);
 
 
 /*
-Woopra Analytics
+GetClicky Analytics
 */
-function woopraReady(tracker) {
-    tracker.setDomain('kickbackcard.com');
-    tracker.setIdleTimeout(300000);
-    tracker.track();
-    return false;
-}
+var clicky_site_ids = clicky_site_ids || [];
+clicky_site_ids.push(66489859);
 (function() {
-    var wsc = document.createElement('script');
-    wsc.src = document.location.protocol+'//static.woopra.com/js/woopra.js';
-    wsc.type = 'text/javascript';
-    wsc.async = true;
-    var ssc = document.getElementsByTagName('script')[0];
-    ssc.parentNode.insertBefore(wsc, ssc);
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//static.getclicky.com/js';
+  ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
 })();
 
 $(function(){
